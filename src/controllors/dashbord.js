7719cff924e303b0fd50ee96a67fbd382e3a2d5d
Luccin Masirika <luccinmasirika@gmail.com>
2021-03-28 18:06:14 +0200

    test

--- src/controllors/dashbord.js
+++ src/controllors/dashbord.js
@@ -4,24 +4,33 @@ const Cartographe = require('../models/Cartographe'); // Fonction Schema
 const Paiement = require('../models/Paiement'); // Fonction Schema
 const PaiementAutre = require('../models/PaiementAutre'); // Fonction Schema
 const Instruction = require('../models/Instruction'); // Fonction Schema
+const moment = require('moment')
+
 
 // Show all Fonctions
 exports.readAll = async (req, res) => {
   const data0 = await Form.countDocuments({});
   const data1 = await Fonction.countDocuments({});
   const data2 = await Cartographe.countDocuments({});
+  const semaine1 = await Cartographe.find({
+    createdAt: {
+      $gte: new Date(),
+      $lt: new Date(2021, 3, 27),
+    },
+  });
   const data3 = await Paiement.find({});
   const data4 = await PaiementAutre.find({});
   const data5 = await Instruction.countDocuments({});
 
   const pMontants = data3.map((n) => n.montant);
-  const pSomme = pMontants.map((n) => (n += n));
+  const paMontants = data4.map((n) => n.montant);
+  const montants = [...pMontants, ...paMontants];
 
   res.json({
     form: data0,
     agent: data1 + data2,
-    pNumber: pMontants.reduce((a, b) => a + b),
-    paNumber: data4.montant,
+    pNumber: eval(montants.join('+')),
     iNumber: data5,
+    test: semaine1,
   });
 };
